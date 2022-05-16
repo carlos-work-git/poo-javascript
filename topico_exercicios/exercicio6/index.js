f100b3641e639365d416ea3e74c797f393c5c5a2
jose-ist <carlos.work@outlook.com>
2022-05-16 15:27:25 -0300

    Exercícios 6

---
+++ topico_exercicios/exercicio6/index.js
@@ -0,0 +1,103 @@
+/**
+ * Crie uma função que retorna se o objeto é uma instancia de outro objeto.
+ * Deve apresentar no console as mensagens de positivo e negativo.
+ */
+
+ //console.log("teste");
+
+function compareObjs(obj1,obj2){
+    // instanceof - objeto, classe
+    if(obj1 instanceof obj2){
+        console.log(`O objeto "${obj1.nome}" é uma instancia de "${obj2.name}"`);
+    } else {
+        console.log(`O objeto "${obj1.nome}" não é uma instancia de "${obj2.name}"`);
+    }
+};
+
+function Inimigo(nome) {
+     this.nome = nome; 
+     this.vivo = true;
+
+     this.ultimaPalavra = () => console.log(`(${this.nome}) - Eu me vingarei!!!`);
+
+   };
+
+
+function Ninja(nome, idade, estilo, nomeProjetil, quantidadeProjeteis) {
+     this.nome = nome; 
+     this.idade = idade;
+     this.estilo = estilo;
+     this.nomeProjetil = nomeProjetil;
+     this.quantidadeProjeteis = quantidadeProjeteis;
+
+     this.golpeFinal = function(){
+        console.log(`(${this.nome}) - Esse é meu jutsu Secreto, toma!`);
+     }
+
+     this.getQuantidadeProjeteis = function  () {
+       // console.log(`${this.nome} tem ${quantidadeProjeteis} ${nomeProjetil}(s)`); 
+       // console.log(Number(this.quantidadeProjeteis));
+        return this.quantidadeProjeteis;
+     };
+
+     this.recarregarProjeteis = function (quantidadeProjeteis){
+        this.quantidadeProjeteis = this.quantidadeProjeteis + quantidadeProjeteis;
+     };
+     
+     this.atirarProjetil = function (Inimigo) {
+
+        if(this.quantidadeProjeteis!=0){
+            console.log(`(${this.nome})- Toma essa ${this.nomeProjetil} !!!`);
+            this.quantidadeProjeteis--;
+            this.getQuantidadeProjeteis();
+
+            danzo.vivo=false;
+            //console.log(danzo);
+            
+            if(danzo.vivo===false){
+                danzo.ultimaPalavra();
+            };
+
+        }else{
+            console.log(`Os projeteis de ${this.nome} acabaram!!!`);
+            return 0;
+        };
+        
+
+
+     };
+
+     this.rendicao = function (){
+         return console.log(`(${this.nome}) - Eu me rendo!`);
+     };
+
+     this.vitoria = function () {
+         console.log(`(${this.nome}) - Esse é meu estilo ninja de ser!!! Ha hasuhauhsuhuahssa`);
+         console.log("");
+         return console.log("¡¡¡ " + this.nome + " - Ganhou a Luta !!!");
+    };
+
+   };
+
+   let danzo = new Inimigo("Danzo")
+   let naruto= new Ninja("Naruto Uzumaki", 14, "Ninjutsu", "Shuriken",3);
+   let kakashi= new Ninja("Kakashi", 30, "Ninjutsu", "kunai",2);
+
+   console.log(naruto);
+   console.log(danzo);
+   console.log("");
+
+   naruto.atirarProjetil(danzo);
+   console.log("");
+   console.log(danzo);
+
+   const nomeClasse = kakashi.constructor.name;
+
+   console.log("");
+   console.log(nomeClasse);
+   compareObjs(naruto, kakashi.constructor);
+
+   console.log("");
+   console.log(danzo.constructor.name);
+   compareObjs(danzo, naruto.constructor);
+
