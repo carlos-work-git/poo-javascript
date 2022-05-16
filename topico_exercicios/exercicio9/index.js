947171df581f03e95de3a1ed013b336ed0a62e88
jose-ist <carlos.work@outlook.com>
2022-05-16 17:14:44 -0300

    Exercícios 9 - Destructuring

---
+++ topico_exercicios/exercicio9/index.js
@@ -0,0 +1,25 @@
+/**
+ * Crie um objeto que tenha características de um caminhão e coloque em propriedades distintas;
+ * Com destructuring coloque essas propriedades em variáveis separadas;
+ */
+
+console.log("teste");
+
+function Caminhao (portas,rodas,capacidadeCarga){
+
+   this.portas = portas;
+   this.rodas = rodas;
+   this.capacidadeCarga = capacidadeCarga;
+
+};
+
+const scania = new Caminhao(2,18,"20T");
+
+console.log(scania);
+
+const {portas, rodas, capacidadeCarga} = scania; // array passado por chaves ao invés de colchetes
+
+console.log(portas);
+console.log(rodas);
+console.log(capacidadeCarga);
+
