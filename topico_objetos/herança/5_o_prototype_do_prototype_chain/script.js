0724e0ec8f56a5f641c5b55020042bf545a3c280
jose-ist <carlos.work@outlook.com>
2022-05-18 15:53:27 -0300

    Modulo 8 - Herança - O problema do prototype chain

---
+++ topico_objetos/herança/5_o_prototype_do_prototype_chain/script.js
@@ -0,0 +1,50 @@
+// "Interface" e "Sobreescrita"
+
+function Veiculo(){}
+
+Veiculo.prototype.carenagem = 'aço';
+Veiculo.prototype.ligar = function(){
+    console.log("O veículo ligou!");
+}
+
+function Trem(tipo){
+    this.tipo=tipo;
+}
+
+Trem.prototype.vagoes=50;
+
+function Carro() {}
+
+Carro.prototype.pneus = 4;
+
+Trem.prototype = Veiculo.prototype; //Copiando o prototype de veículo para trem
+Carro.prototype = Veiculo.prototype; //Copiando o prototype de veículo para carro
+
+let trembala = new Trem('Trem Bala');
+
+console.log(trembala.tipo);
+trembala.ligar();
+
+let ferrari = new Carro();
+ferrari.ligar();
+
+let trem = new Trem('Trem');
+trem.ligar();
+
+console.log(trem.carenagem);
+
+Carro.prototype.ligar = function(){ //Sobreescrevendo o método ligar() da funcção veículo.
+    console.log("O carro ligou");
+}
+ferrari.ligar();
+trem.ligar();
+trembala.ligar();
+
+console.log(trembala instanceof Trem); //True
+console.log(ferrari instanceof Carro); //True
+
+console.log(trembala instanceof Veiculo); //True
+console.log(ferrari instanceof Veiculo);  //True
+
+console.log(trembala instanceof Object); //True
+console.log(ferrari instanceof Object);  //True
