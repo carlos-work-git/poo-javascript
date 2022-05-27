86655a22ad75d5b4c662f64d4eb53c6f09df291a
jose-ist <carlos.work@outlook.com>
2022-05-27 17:25:57 -0300

    Módulo 11 - Introdução ao Typescript - Aula 6 Objetos e Arrays

---
+++ TypeScript/3_arrays_e_objetos/app.js
@@ -0,0 +1,12 @@
+// primeira opção
+var frutas = ['banana', 'maçã', 'laranja'];
+//Com Generics
+var numeros = [1, 2, 3, 4, 5, 6];
+console.log(frutas);
+console.log(numeros);
+var obj = {
+    name: 'Mateus',
+    age: 29
+};
+console.log(obj);
+console.log(obj.name);
--- TypeScript/3_arrays_e_objetos/app.ts
+++ TypeScript/3_arrays_e_objetos/app.ts
@@ -1,1 +1,16 @@
+// primeira opção
+const frutas: string[] = ['banana', 'maçã', 'laranja'];
 
+//Com Generics
+const numeros: Array<number> = [1,2,3,4,5,6];
+
+console.log(frutas);
+console.log(numeros);
+
+let obj = {
+    name: 'Mateus',
+    age: 29
+}
+
+console.log(obj);
+console.log(obj.name);
