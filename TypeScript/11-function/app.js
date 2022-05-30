cfa8cfa9797cecd5f01032227d81ae7cfc2fb752
jose-ist <carlos.work@outlook.com>
2022-05-30 15:54:26 -0300

    Módulo 11 - Introdução ao Typescript - Aula 15 function

---
+++ TypeScript/11-function/app.js
@@ -0,0 +1,4 @@
+function cumprimento(saudacao, nome) {
+    return "Ol\u00E1 ".concat(saudacao, " ").concat(nome, ", tudo bem?");
+}
+console.log(cumprimento("Sr.", "Matheus"));
---
+++ TypeScript/11-function/app.ts
@@ -0,0 +1,7 @@
+function cumprimento(saudacao: string, nome: string): string { // :return
+    return `Olá ${saudacao} ${nome}, tudo bem?`
+}
+
+console.log(cumprimento("Sr.", "Matheus"));
+
+
