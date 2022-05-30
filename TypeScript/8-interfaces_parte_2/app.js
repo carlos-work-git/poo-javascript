3e98064a8aec6ab23e36eda344437cef3f9571ea
jose-ist <carlos.work@outlook.com>
2022-05-30 14:03:48 -0300

    Módulo 11 - Introdução ao Typescript - Aula 12 interface-parte-2

---
+++ TypeScript/8-interfaces_parte_2/app.js
@@ -0,0 +1,18 @@
+function criarCarro(config) {
+    var carro = { marca: 'Default', aro: 16, tetoSolar: false, cor: "Preto" };
+    if (config.marca) {
+        carro.marca = config.marca;
+    }
+    if (config.aro) {
+        carro.aro = config.aro;
+    }
+    if (config.tetoSolar) {
+        carro.tetoSolar = config.tetoSolar;
+    }
+    if (config.cor) {
+        carro.cor = config.cor;
+    }
+    return carro;
+}
+var bmw = criarCarro({ marca: "BMW", tetoSolar: true });
+console.log(bmw);
