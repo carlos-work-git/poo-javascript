598ceaf670fa7786cb8305f307ca108fec722887
jose-ist <carlos.work@outlook.com>
2022-05-30 10:21:04 -0300

    Módulo 11 - Introdução ao Typescript - Aula 8 Enum

---
+++ TypeScript/5_enum/app.js
@@ -0,0 +1,9 @@
+var Barco;
+(function (Barco) {
+    Barco[Barco["Motor"] = 2] = "Motor";
+    Barco[Barco["Proa"] = 1] = "Proa";
+    Barco[Barco["Popa"] = 1] = "Popa";
+})(Barco || (Barco = {}));
+;
+var numeroDeMotores = Barco.Motor;
+console.log("Barco.Motor " + Barco.Motor);
---
+++ TypeScript/5_enum/app.ts
@@ -0,0 +1,5 @@
+enum Barco {Motor=2, Proa=1, Popa=1};
+
+let numeroDeMotores= Barco.Motor;
+
+console.log("Barco.Motor " + Barco.Motor);
