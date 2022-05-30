b9157d21de2748fc2921e149c664004ab82ee527
jose-ist <carlos.work@outlook.com>
2022-05-30 11:38:15 -0300

    Módulo 11 - Introdução ao Typescript - Aula 9 Any

---
+++ TypeScript/6_any/app.ts
@@ -0,0 +1,11 @@
+let naoSabemos: any;
+
+naoSabemos = 'teste';
+naoSabemos = 3;
+naoSabemos = true;
+
+console.log(naoSabemos);
+
+let listaIndefinida: any[] = [1, 'teste', true];
+
+console.log(listaIndefinida);
